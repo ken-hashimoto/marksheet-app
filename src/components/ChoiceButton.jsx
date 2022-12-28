2747fa1337d228cb956cb41fa541366752745547
hashimoto-k <hashimoto.kenji.1102@gmail.com>
2022-12-29 00:23:25 +0900

    useStateでマークシートを塗りつぶせるようにした

--- src/components/ChoiceButton.jsx
+++ src/components/ChoiceButton.jsx
@@ -1,20 +1,26 @@
 import { useState } from "react";
 import styled from "styled-components";
 
-const StyledChoiceButton = styled.div`
-  width: 15px;
-  height: 30px;
-  border-radius: 50%;
-  border: 2px solid;
-  box-sizing: border-box;
-`;
 export const ChoiceButton = () => {
-  const [isfilled, setFilled] = useState(false);
+  const StyledChoiceButton = styled.div`
+    width: 15px;
+    height: 30px;
+    border-radius: 50%;
+    border: 2px solid;
+    box-sizing: border-box;
+    &.is-filled {
+      background: black;
+    }
+  `;
+  const [isFilled, setFilled] = useState(false);
   const onClickChoiceButton = () => {
-    console.log(isfilled);
-    setFilled(!isfilled);
+    console.log(isFilled);
+    setFilled(!isFilled);
   };
   return (
-    <StyledChoiceButton onClick={onClickChoiceButton}></StyledChoiceButton>
+    <StyledChoiceButton
+      onClick={onClickChoiceButton}
+      className={isFilled ? "is-filled" : ""}
+    ></StyledChoiceButton>
   );
 };
