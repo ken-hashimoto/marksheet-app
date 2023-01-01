b791cb98c32ccd9aa8136f11210e84e9b69f0fd9
hashimoto-k <hashimoto.kenji.1102@gmail.com>
2023-01-01 18:32:14 +0900

    minimumで完成

--- src/components/ChoiceButtons.jsx
+++ src/components/ChoiceButtons.jsx
@@ -31,9 +31,11 @@ const ChoiceButton = (props) => {
 };
 
 export const ChoiceButtons = (props) => {
-  const cnt = props.cnt;
-  const range = (start, end) => [...Array(end + 1).keys()].slice(start);
-  const num_list = range(1, cnt);
+  const { cnt, format } = props;
+  const num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const alphabet_list = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
+  const choice_list =
+    format === "number" ? num_list.slice(0, cnt) : alphabet_list.slice(0, cnt);
   const SChoiceButtonsWrapper = styled.div`
     display: flex;
     border: 2px solid;
@@ -42,9 +44,23 @@ export const ChoiceButtons = (props) => {
   `;
   return (
     <SChoiceButtonsWrapper>
-      {num_list.map((index) => (
-        <ChoiceButton index={index}></ChoiceButton>
+      {choice_list.map((val) => (
+        <ChoiceButton index={val}></ChoiceButton>
       ))}
     </SChoiceButtonsWrapper>
+  );
+};
+
+export const ChoiceButtonList = (props) => {
+  const { QNum, cnt, format } = props;
+  const generateQNumList = (start, stop) =>
+    Array.from({ length: stop - start + 1 }, (_, i) => start + i);
+  const QNumList = generateQNumList(1, QNum);
+  return (
+    <>
+      {QNumList.map((val) => (
+        <ChoiceButtons cnt={cnt} format={format}></ChoiceButtons>
+      ))}
+    </>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,9 @@
 // import styled from "styled-components";
 import { useState } from "react";
-import { ChoiceButtons } from "../components/ChoiceButtons";
+import { ChoiceButtonList } from "../components/ChoiceButtons";
 import Select from "react-select";
 // 問題数の選択肢のプルダウンを生成
-const generateChoiceNumOptions = () => {
-  const maxNum = 9;
+const generateNumSelect = (maxNum) => {
   const options = [];
   for (let i = 1; i < maxNum + 1; i++) {
     const tmp = { value: i, key: i, label: String(i) };
@@ -13,33 +12,67 @@ const generateChoiceNumOptions = () => {
   return options;
 };
 
-const SelectChoiceFormat = () => {
+const SelectChoiceFormat = ({ value, handler }) => {
   const options = [
-    { value: "alphabet", label: "アルファベット（A,B,C, ...）" },
     { value: "number", label: "数字（1,2,3, ...）" },
+    { value: "alphabet", label: "アルファベット（A,B,C, ...）" },
   ];
-  return <Select options={options} />;
+  const handleChoiceFormat = (e) => {
+    handler((value) => (value = e.value));
+  };
+  return (
+    <Select
+      options={options}
+      onChange={handleChoiceFormat}
+      defaultValue={options[0]}
+    />
+  );
 };
 const SelectChoiceNum = ({ value, handler }) => {
-  const options = generateChoiceNumOptions();
+  const options = generateNumSelect(9);
   const handleChoiceNum = (e) => {
-    handler(e.value);
+    handler((value) => (value = e.value));
   };
-  return <Select options={options} onChange={handleChoiceNum} />;
+  return (
+    <Select
+      options={options}
+      onChange={handleChoiceNum}
+      defaultValue={options[5 - 1]}
+    />
+  );
+};
+
+const SelectQNum = ({ value, handler }) => {
+  const options = generateNumSelect(200);
+  const handleQNum = (e) => {
+    handler((value) => (value = e.value));
+  };
+  return <Select options={options} onChange={handleQNum} />;
 };
 export const Home = () => {
-  const [ChoiceNum, setChoiceNum] = useState(5);
-
+  const initialQNum = 5;
+  const [ChoiceNum, setChoiceNum] = useState(initialQNum);
+  const [ChoiceFormat, setChoiceFormat] = useState("number");
+  const [QNum, setQNum] = useState(5);
   return (
     <>
       <h2>問題番号の形式を選択してください</h2>
-      <SelectChoiceFormat></SelectChoiceFormat>
+      <SelectChoiceFormat
+        value={ChoiceFormat}
+        handler={setChoiceFormat}
+      ></SelectChoiceFormat>
       <h2>選択肢の数(1から9まで)を入力してください</h2>
       <SelectChoiceNum
         value={ChoiceNum}
         handler={setChoiceNum}
       ></SelectChoiceNum>
-      <ChoiceButtons cnt={ChoiceNum}></ChoiceButtons>
+      <h2>問題数を入力してください</h2>
+      <SelectQNum value={QNum} handler={setQNum}></SelectQNum>
+      <ChoiceButtonList
+        cnt={ChoiceNum}
+        format={ChoiceFormat}
+        QNum={QNum}
+      ></ChoiceButtonList>
     </>
   );
 };
